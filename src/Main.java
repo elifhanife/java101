f99c7d295b981a2ee1169dca93f3578d1ba2f6c6
elifhanife <hanifelif52@gmail.com>
2023-12-20 22:11:24 +0300

    not ortalaması hesaplama

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int mat, fizik, kimya, turkce, tarih, muzik;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Matematik notunu girin: ");
+        mat = scan.nextInt();
+        System.out.print("Fizik notunu girin: ");
+        fizik = scan.nextInt();
+        System.out.print("Kimya notunu girin: ");
+        kimya = scan.nextInt();
+        System.out.print("Türkçe notunu girin: ");
+        turkce = scan.nextInt();
+        System.out.print("Tarih notunu girin: ");
+        tarih = scan.nextInt();
+        System.out.print("Müzik notunu girin: ");
+        muzik = scan.nextInt();
+
+        int toplam = mat + fizik + kimya + turkce + tarih + muzik;
+        double ortalama = toplam / 6.0;
+        System.out.println( ortalama >= 60 ? "Sınıfı Geçti" : "Sınıfta Kaldı");
+        }
+    }
